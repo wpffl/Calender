c1a21cc4846a3d8274ad01d896751d454ece58d7
wpffl <wpffl3333@naver.com>
2020-12-16 18:18:08 +0900

    create UserVO and ContentVO

--- src/main/java/com/gukje/sch/common/ContentVO.java
+++ src/main/java/com/gukje/sch/common/ContentVO.java
@@ -1,5 +1,84 @@
 package com.gukje.sch.common;
 
-public class ContentVO {
+import java.sql.Date;
 
+public class ContentVO {
+	private String nickname;
+	private String type;
+	private String title;
+	private String content;
+	private int year;
+	private int month;
+	private int day;
+	private int hour;
+	private int minute;
+	private Date regdate;
+	private String locker;
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getDay() {
+		return day;
+	}
+	public void setDay(int day) {
+		this.day = day;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public String getLocker() {
+		return locker;
+	}
+	public void setLocker(String locker) {
+		this.locker = locker;
+	}
+	
 }
